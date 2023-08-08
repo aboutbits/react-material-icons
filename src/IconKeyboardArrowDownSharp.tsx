7eb0b00d7949798168352b7d2d2aaa4a8d64fd09
Alex Lanz <alex.lanz@aboutbits.it>
2023-08-08 14:39:04 +0200

    update icons

--- src/IconKeyboardArrowDownSharp.tsx
+++ src/IconKeyboardArrowDownSharp.tsx
@@ -5,7 +5,7 @@ const IconKeyboardArrowDownSharp: React.FC<IconProps> = ({ ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
     {props.title && <title>{props.title}</title>}
     <path d="M0 0h24v24H0V0z" fill="none" />
-    <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
+    <path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
   </svg>
 )
 
