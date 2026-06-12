32db4f7aea775faa2a05cea7b0715b4134b42ce0
Lukas Weiss <lukas.weiss@aboutbits.it>
2026-06-12 11:41:38 +0200

    remove custom eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,21 +6,6 @@ export default defineConfig([
   ts,
   jest,
   {
-    rules: {
-      'import/namespace': 'off',
-      'import/order': 'error',
-      '@typescript-eslint/no-var-requires': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-    },
-  },
-  {
     ignores: ['dist', 'node_modules'],
   },
 ])
